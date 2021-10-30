9cb447016ea9093c1283ae5a781f30a342053a34
Erme <erme.schultz@commcepta.com.br>
2021-10-30 09:33:50 -0300

    foward API params appending it to existent search params

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
+  const { searchParams } = new URL(request.url)
+
+  searchParams.append(API_KEY_NAME, API_KEY_VALUE)
+
   return new Response('Hello worker!', {
     headers: { 'content-type': 'text/plain' },
   })
