08c3f596aa4648ee3ecfc263eff1c02c689f7a02
Erme <erme.schultz@commcepta.com.br>
2021-10-30 11:59:38 -0300

    handles empty search params and also status codes other than 200

--- index.js
+++ index.js
@@ -8,23 +8,29 @@ addEventListener('fetch', event => {
 async function handleRequest(request) {
   const { searchParams } = new URL(request.url)
 
-  const cacheKey = `${searchParams}`
+  const cacheKey = (`${searchParams}` !== "") ? `${searchParams}` : null
 
   searchParams.append(API_KEY_NAME, API_KEY_VALUE)
 
-  const cachedValue = await KV_CACHE.get(cacheKey)
+  const cachedValue = (cacheKey) ? await KV_CACHE.get(cacheKey) : null
+  
   let response 
+  let status = 200
 
   if(cachedValue){
     response = cachedValue
   }else{
     const apiResponse = await fetch(`${API_BASE_URL}?${searchParams}`)
     response = JSON.stringify(await apiResponse.json())
-    KV_CACHE.put(cacheKey, response, { expirationTtl: 120 })
+    if(apiResponse.status == 200){
+      KV_CACHE.put(cacheKey, response, { expirationTtl: 120 })     
+    }else{
+      status = apiResponse.status
+    }
   }
   
   return new Response(response, {
-    status: 200,
+    status: status,
     headers: {
       "content-type": "application/json"
     }
